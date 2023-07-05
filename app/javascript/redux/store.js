4c9bea5fb1ce7950996df8c9e8a32cce96b3cfbf
Malik Ghulam Subbhani <gsmalik030@gmail.com>
2023-07-05 13:18:51 +0200

    Added redux store and placesSlice

---
+++ app/javascript/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import placesReducer from "./places/placesSlice"
+const store = configureStore({
+    reducer:{
+        places: placesReducer
+    }
+})
+
+export default store;
