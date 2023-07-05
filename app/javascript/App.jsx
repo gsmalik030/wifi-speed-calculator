a9a2d2ba44cbe501168524a1716577534abd55e9
Malik Ghulam Subbhani <gsmalik030@gmail.com>
2023-07-05 14:45:42 +0200

    Adding search functionality

--- app/javascript/App.jsx
+++ app/javascript/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from "./redux/store"
+import store from './redux/store';
 import Places from './components/Places';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
+  <Provider store={store}>
     <Places />
-    </Provider>
+  </Provider>
 );
 
 export default App;
--- app/javascript/components/Places.jsx
+++ app/javascript/components/Places.jsx
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { styled } from '@mui/material/styles';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import Button from '@mui/material/Button';
+import SearchIcon from '@mui/icons-material/Search';
+
 import Table from '@mui/material/Table';
 import { Container } from '@mui/material';
 import TableBody from '@mui/material/TableBody';
@@ -10,6 +18,46 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { getPlacesData } from '../redux/places/placesSlice';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
@@ -36,6 +84,8 @@ const Places = () => {
     dispatch(getPlacesData())
   },[])
 
+  const [search, setSearch] = useState('')
+
   const {places, isLoading} = useSelector((store) => store.places)
 
   function createData(name, city, most_recent_speed, speed_unit, avg_speed) {
@@ -54,6 +104,31 @@ const Places = () => {
     return <div>Loading...</div>;
   }
   return (
+    <>
+        <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+            <Typography variant='h4' component= "h2">
+                Wifi Speed Tracker
+            </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: 'flex', gap: '15px' }}>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          <Button variant="contained" color="success">
+        New Test
+        </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
     <Container>
           <TableContainer component={Paper}>
     <Table sx={{ minWidth: 700 }} aria-label="customized table">
@@ -82,6 +157,7 @@ const Places = () => {
     </Table>
   </TableContainer>
     </Container>
+    </>
   );
 };
 
